cfa4ffd42337d591d9de3c9324c55f9d02b46f55
Ray Kugler <raymond.kugler@gmail.com>
2018-09-28 10:00:27 -0700

    link and edge fixes

--- src/components/Atmos_Button_Holder.js
+++ src/components/Atmos_Button_Holder.js
@@ -10,12 +10,13 @@ class Atmos_Button_Holder extends React.Component{
     render(){
         return(
         
-<nav className='atmos_button_holder' >
+
+<a href="https://www.raykugler.com" target="_blank" className='atmos_button_holder'>
 <img src='.\images\atmos_button_left.png' className='atmos_button_left'/>
 <img src='.\images\atmos_button_right.png' className='atmos_button_right'/>
 
 <h3 className='atmos_button_text'>ray kugler</h3>
-</nav>
+</a>
 
         )}}
 
